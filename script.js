c3b57b437db037c2b33f7206cf7e0d1655cfc866
singledeveloper63 <singledeveloper63@gmail.com>
2021-11-23 02:31:16 +0500

    Asdasdf

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const myId = Math.floor(Math.random() * 100);
 const socket = io("https://guarded-river-30617.herokuapp.com");
 
 socket.on("audio-input", ({ id, buffer }) => {
-  var blob = new Blob([buffer], { type: "audio/ogg; codecs=opus" });
-  var audio = document.createElement("audio");
-  audio.src = window.URL.createObjectURL(blob);
-  audio.play();
+  if (id !== myId) {
+    var blob = new Blob([buffer], { type: "audio/ogg; codecs=opus" });
+    var audio = document.createElement("audio");
+    audio.src = window.URL.createObjectURL(blob);
+    audio.play();
+  }
 });
+let recordStart = false;
 
 console.log(window.navigator.getUserMedia);
 
@@ -34,11 +37,16 @@ function startStream() {
         socket.emit("stream", { id: myId, data: blob });
       };
 
-      document.querySelector("button").addEventListener("mousedown", () => {
-        recorder.start();
-      });
-      document.querySelector("button").addEventListener("mouseup", () => {
-        recorder.stop();
+      document.querySelector("button").addEventListener("click", (e) => {
+        if (!recordStart) {
+          e.target.classList.add("active");
+          recorder.start();
+          recordStart = true;
+        } else {
+          e.target.classList.remove("active");
+          recorder.stop();
+          recordStart = false;
+        }
       });
     })
     .catch((error) => {
